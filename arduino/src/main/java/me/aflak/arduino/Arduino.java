93cf4bd0e665f74c82fd417546d73aa0d7143bbe
Omar Aflak <aflakomar@gmail.com>
2018-05-09 16:41:29 +0200

    added 0x0403 vendor id

--- arduino/src/main/java/me/aflak/arduino/Arduino.java
+++ arduino/src/main/java/me/aflak/arduino/Arduino.java
@@ -8,17 +8,19 @@ import android.content.IntentFilter;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
+import android.util.Log;
 
 import com.felhr.usbserial.UsbSerialDevice;
 import com.felhr.usbserial.UsbSerialInterface;
 
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Omar on 21/05/2017.
  */
 
-public class Arduino implements UsbSerialInterface.UsbReadCallback{
+public class Arduino implements UsbSerialInterface.UsbReadCallback {
     private Context context;
     private ArduinoListener listener;
 
@@ -32,18 +34,18 @@ public class Arduino implements UsbSerialInterface.UsbReadCallback{
     private boolean isOpened;
 
     private static final String ACTION_USB_DEVICE_PERMISSION = "me.aflak.arduino.USB_PERMISSION";
-    private static final int ARDUINO_VENDOR_ID = 9025;
+    private static final int[] ARDUINO_VENDOR_IDS = {0x9025, 0x0403};
     private static final int DEFAULT_BAUD_RATE = 9600;
 
     public Arduino(Context context, int baudRate) {
         init(context, baudRate);
     }
 
-    public Arduino(Context context){
+    public Arduino(Context context) {
         init(context, DEFAULT_BAUD_RATE);
     }
 
-    private void init(Context context, int baudRate){
+    private void init(Context context, int baudRate) {
         this.context = context;
         this.usbReceiver = new UsbReceiver();
         this.usbManager = (UsbManager) context.getSystemService(Context.USB_SERVICE);
@@ -51,7 +53,7 @@ public class Arduino implements UsbSerialInterface.UsbReadCallback{
         this.isOpened = false;
     }
 
-    public void setArduinoListener(ArduinoListener listener){
+    public void setArduinoListener(ArduinoListener listener) {
         this.listener = listener;
 
         IntentFilter intentFilter = new IntentFilter();
@@ -61,16 +63,16 @@ public class Arduino implements UsbSerialInterface.UsbReadCallback{
         context.registerReceiver(usbReceiver, intentFilter);
 
         lastArduinoAttached = getAttachedArduino();
-        if(lastArduinoAttached!=null && listener!=null){
+        if (lastArduinoAttached != null && listener != null) {
             listener.onArduinoAttached(lastArduinoAttached);
         }
     }
 
-    public void unsetArduinoListener(){
+    public void unsetArduinoListener() {
         this.listener = null;
     }
 
-    public void open(UsbDevice device){
+    public void open(UsbDevice device) {
         PendingIntent permissionIntent = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_USB_DEVICE_PERMISSION), 0);
         IntentFilter filter = new IntentFilter();
         filter.addAction(ACTION_USB_DEVICE_PERMISSION);
@@ -79,15 +81,15 @@ public class Arduino implements UsbSerialInterface.UsbReadCallback{
         usbManager.requestPermission(device, permissionIntent);
     }
 
-    public void reopen(){
+    public void reopen() {
         open(lastArduinoAttached);
     }
 
-    public void close(){
-        if(serialPort!=null){
+    public void close() {
+        if (serialPort != null) {
             serialPort.close();
         }
-        if(connection!=null){
+        if (connection != null) {
             connection.close();
         }
 
@@ -95,8 +97,8 @@ public class Arduino implements UsbSerialInterface.UsbReadCallback{
         context.unregisterReceiver(usbReceiver);
     }
 
-    public void send(byte[] bytes){
-        if(serialPort!=null){
+    public void send(byte[] bytes) {
+        if (serialPort != null) {
             serialPort.write(bytes);
         }
     }
@@ -105,21 +107,21 @@ public class Arduino implements UsbSerialInterface.UsbReadCallback{
         @Override
         public void onReceive(Context context, Intent intent) {
             UsbDevice device;
-            if(intent.getAction()!=null) {
+            if (intent.getAction() != null) {
                 switch (intent.getAction()) {
                     case UsbManager.ACTION_USB_DEVICE_ATTACHED:
                         device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
-                        if (device.getVendorId() == ARDUINO_VENDOR_ID) {
+                        if (hasId(device.getVendorId())) {
                             lastArduinoAttached = device;
-                            if(listener != null){
+                            if (listener != null) {
                                 listener.onArduinoAttached(device);
                             }
                         }
                         break;
                     case UsbManager.ACTION_USB_DEVICE_DETACHED:
                         device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
-                        if (device.getVendorId() == ARDUINO_VENDOR_ID) {
-                            if(listener != null){
+                        if (hasId(device.getVendorId())) {
+                            if (listener != null) {
                                 listener.onArduinoDetached();
                             }
                         }
@@ -127,7 +129,7 @@ public class Arduino implements UsbSerialInterface.UsbReadCallback{
                     case ACTION_USB_DEVICE_PERMISSION:
                         if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
                             device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
-                            if (device.getVendorId() == ARDUINO_VENDOR_ID) {
+                            if (hasId(device.getVendorId())) {
                                 connection = usbManager.openDevice(device);
                                 serialPort = UsbSerialDevice.createUsbSerialDevice(device, connection);
                                 if (serialPort != null) {
@@ -141,14 +143,13 @@ public class Arduino implements UsbSerialInterface.UsbReadCallback{
 
                                         isOpened = true;
 
-                                        if(listener != null){
+                                        if (listener != null) {
                                             listener.onArduinoOpened();
                                         }
                                     }
                                 }
                             }
-                        }
-                        else if(listener!=null){
+                        } else if (listener != null) {
                             listener.onUsbPermissionDenied();
                         }
                         break;
@@ -157,10 +158,10 @@ public class Arduino implements UsbSerialInterface.UsbReadCallback{
         }
     }
 
-    private UsbDevice getAttachedArduino(){
+    private UsbDevice getAttachedArduino() {
         HashMap<String, UsbDevice> map = usbManager.getDeviceList();
-        for (UsbDevice device : map.values()){
-            if (device.getVendorId()==ARDUINO_VENDOR_ID){
+        for (UsbDevice device : map.values()) {
+            if (hasId(device.getVendorId())) {
                 return device;
             }
         }
@@ -169,12 +170,22 @@ public class Arduino implements UsbSerialInterface.UsbReadCallback{
 
     @Override
     public void onReceivedData(byte[] bytes) {
-        if(listener != null && bytes.length!=0){
+        if (listener != null && bytes.length != 0) {
             listener.onArduinoMessage(bytes);
         }
     }
 
     public boolean isOpened() {
         return isOpened;
+    }
+
+    private boolean hasId(int id) {
+        Log.d("TEST ID", id+"");
+        for(int i=0  ; i<ARDUINO_VENDOR_IDS.length ; i++){
+            if(ARDUINO_VENDOR_IDS[i]==id){
+                return true;
+            }
+        }
+        return false;
     }
 }
